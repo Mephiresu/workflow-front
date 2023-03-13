c07cc29e8eae2e7e2d916da9de0d25584b192e15
Mephiresu <67890890+Mephiresu@users.noreply.github.com>
2023-03-14 03:16:32 +0500

    feat/settings: modal window component

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="absolute z-50 inset-0 bg-black bg-opacity-50 flex flex-row items-center justify-center"
+    @click.self="$emit('close')">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  emits: ['close'],
+})
+</script>
