97f3b30c900ceae566059faff4f31ef3bfb1838f
Mephiresu <67890890+Mephiresu@users.noreply.github.com>
2023-03-14 00:38:09 +0500

    feat/settings: save panel

---
+++ src/components/SavePanel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="fixed bottom-0 left-0 right-0 p-4 bg-gray-200 space-x-4 border-gray-300 border-t">
+    <AppButton class="w-20" size="sm" @click="$emit('save')">Save</AppButton>
+    <AppButton
+      class="w-20"
+      size="sm"
+      variant="secondary"
+      @click="$emit('revert')"
+      >Revert</AppButton
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({ emits: ['save', 'revert'] })
+</script>
